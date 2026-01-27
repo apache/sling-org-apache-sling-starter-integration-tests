fc685b9106093b6f376a3615418708b35da0ce84
Robert Munteanu <rombert@apache.org>
2026-01-27 14:06:14 +0100

    SLING-13071 - HTL use objects that adapt from the javax SlingHttpServletRequest cannot be instantiated a Jakarta context (#41)
    Add a disabled integration test demonstrating the problem

--- src/main/java/org/apache/sling/starter/webapp/integrationtest/scripting/htl/HtlTest.java
+++ src/main/java/org/apache/sling/starter/webapp/integrationtest/scripting/htl/HtlTest.java
@@ -89,4 +89,26 @@ public class HtlTest extends HttpTestBase {
 
         assertTrue("Expected content to contain 'from-sling-model'", content.contains("from-sling-model"));
     }
+
+    // enable once SLING-13071 is fixed
+    public void disabledTestScriptWithAdapterFactoryFromJavaxServlet() throws IOException {
+
+        testClient.mkdirs(HTTP_BASE_URL, "/apps/sling/test/htl/requesthashcode");
+        testClient.mkdirs(HTTP_BASE_URL, "/content/htl");
+
+        testClient.upload(
+                HTTP_BASE_URL + "/apps/sling/test/htl/requesthashcode/requesthashcode.html",
+                getClass().getResourceAsStream("/integration-test/htl/requesthashcode.html"));
+
+        testClient.createNode(
+                HTTP_BASE_URL + "/content/htl/request-hashcode",
+                Collections.singletonMap("sling:resourceType", "sling/test/htl/requesthashcode"));
+
+        String content =
+                getContent(HTTP_BASE_URL + "/content/htl/request-hashcode.html", CONTENT_TYPE_DONTCARE, null, 200);
+
+        assertTrue(
+                "Expected content to contain 'Request hash code:' followed by a hash code",
+                content.matches("(?s).*Request hash code:\\s*\\d+.*"));
+    }
 }
